18dba2f0aad33d1fb306f5be993f0478d42f42ac
biancavictorino <biancamarie.victorino@gmail.com>
2024-06-21 16:26:20 +0800

    modified hero-section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const Hero = () => {
+  
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    element.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className='px-8 h-screen flex flex-col justify-center bg-grid-pattern gap-3 lg:px-28'>
         <div className='text-3xl font-bold lg:text-5xl'>
@@ -9,7 +15,7 @@ const Hero = () => {
         </div>
         <div>
           <p className='text-md leading-7 lg:text-md lg:w-1/2'>
-            Weaving <a href="#codes" className='underline decoration-solid text-primary-color'>beautiful design</a> with <a href="#codes" className='underline decoration-solid text-primary-color'>functional code</a> is my creative passion. I love crafting interactive websites that not only look stunning, but also engage users and deliver a seamless experience.
+            Weaving <a onClick={() => scrollToSection('designs')} className='underline decoration-solid text-primary-color cursor-pointer'>beautiful design</a> with <a onClick={() => scrollToSection('works')} className='underline decoration-solid text-primary-color cursor-pointer'>functional code</a> is my creative passion. I love crafting interactive websites that not only look stunning, but also engage users and deliver a seamless experience.
           </p>
         </div>
         <div className='absolute inset-0 bg-gradient-overlay'></div>
